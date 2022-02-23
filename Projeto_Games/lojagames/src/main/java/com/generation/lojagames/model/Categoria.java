075808c9da56a1c0f83c7822205d8246a1683812
unknown <laralourenco5966@gmail.com>
2022-02-23 20:06:21 -0300

    Projeto Spring - Loja de games

---
+++ Projeto_Games/lojagames/src/main/java/com/generation/lojagames/model/Categoria.java
@@ -0,0 +1,50 @@
+package com.generation.lojagames.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+//criação de tabela
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+	//criação da chave primária e auto_increment
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O campo categoria é obrigátorio")
+	private String descricao;
+	
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
+	private List <Produto> produto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+}
---
+++ Projeto_Games/lojagames/src/main/java/com/generation/lojagames/model/Usuario.java
@@ -0,0 +1,73 @@
+package com.generation.lojagames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table (name = "tb_usuario")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O campo nome é obrigatório")
+	private String nome;
+	
+	@NotBlank(message = "O campo email é obrigatório")
+	@Size(min = 3, max =100, message = "O campo email deve ser maior que 3 e menor que 100 caracteres")
+	private String email;
+	
+	@NotBlank(message = "O campo senha é obrigatório")
+	@Size(min = 3, max =8, message = "O campo senha deve ser maior que 3 e menor que 8 caracteres")
+	private String senha;
+	
+	private String foto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	
+	
+}
